2eec69cbfc871754933da2238411cd5cdc11a373
karthikbest <60345326+karthikbest@users.noreply.github.com>
2025-01-13 08:51:40 -0500

    Toast added & other UI changes

--- StockPredictor/Models/PredictionResponse.cs
+++ StockPredictor/Models/PredictionResponse.cs
@@ -2,21 +2,21 @@
 {
     public class PredictionResponse
     {
-        public string CompanyName { get; set; } // Matches "CompanyName" in the JSON
-        public LastClosingPriceModel LastClosingPrice { get; set; } // Matches "LastClosingPrice" in the JSON
-        public List<PredictedPriceModel> PredictedPrices { get; set; } // Matches "PredictedPrices" in the JSON
-        public string Recommendation { get; set; } // Matches "Recommendation" in the JSON
+        public string CompanyName { get; set; } 
+        public LastClosingPriceModel LastClosingPrice { get; set; } 
+        public List<PredictedPriceModel> PredictedPrices { get; set; } 
+        public string Recommendation { get; set; } 
     }
 
     public class LastClosingPriceModel
     {
-        public string Date { get; set; } // Matches "Date" in "LastClosingPrice"
-        public double Price { get; set; } // Matches "Price" in "LastClosingPrice"
+        public string Date { get; set; } 
+        public double Price { get; set; } 
     }
 
     public class PredictedPriceModel
     {
-        public string Date { get; set; } // Matches "Date" in "PredictedPrices"
-        public double Price { get; set; } // Matches "Price" in "PredictedPrices"
+        public string Date { get; set; } 
+        public double Price { get; set; } 
     }
 }
